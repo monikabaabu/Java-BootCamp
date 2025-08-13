51ec2c5c5b1568ba5a67ce31b4e98e558aa82192
monikabaabu <monikababu207@gmail.com>
2025-08-13 13:59:31 +0530

    recursion

--- problems/recursion/CountZeros.java
+++ problems/recursion/CountZeros.java
@@ -1,23 +1,29 @@
 package recursion;
 import java.util.*;
 public class CountZeros {
-    static int count(int n){
-        return zero(n,0);
+    static int count(int n) {
+        return zero(n, 0);
     }
-    private static int zero(int n,int c){
-        if(n==0){
+
+    private static int zero(int n, int c) {
+        if (n == 0) {
             return c;
         }
-        int rem = n%10;
-        if(rem==0) {
+        int rem = n % 10;
+        if (rem == 0) {
             return zero(n / 10, c + 1);
         }
-        return zero(n/10,c);
+        return zero(n / 10, c);
     }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("enter number:");
         int n = sc.nextInt();
-        System.out.println(count(n));
+        if (n == 0) {
+            System.out.println("1");
+        } else {
+            System.out.println(count(n));
+        }
     }
 }
