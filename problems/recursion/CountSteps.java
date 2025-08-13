2e52035760d276929002d5079ec401b4e20b227a
monikabaabu <monikababu207@gmail.com>
2025-08-13 14:17:42 +0530

    LeetCode-1342

---
+++ problems/recursion/CountSteps.java
@@ -0,0 +1,23 @@
+//1342. Number of Steps to Reduce a Number to Zero
+package recursion;
+import java.util.*;
+public class CountSteps {
+    static int steps(int n, int c ){
+        if(n==0){
+            return c;
+        }
+        if(n%2==0){
+            return steps(n/2,c+1);
+        }
+        return steps(n-1,c+1);
+    }
+    public static int numberOfSteps(int num) {
+        return steps(num,0);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the number");
+        int n = sc.nextInt();
+        System.out.println(numberOfSteps(n));
+    }
+}
